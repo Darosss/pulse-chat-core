e9dd5439c8895ed6058572111d68421a9e10fcd3
Darosss <darosss12@proton.me>
2026-08-19 14:01:32 +0200

    feat(message): integrate dotenv for environment variable management

--- apps/message/Program.cs
+++ apps/message/Program.cs
@@ -1,11 +1,23 @@
 
+using dotenv.net;
 using message.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load(new(envFilePaths:["../../.env"])
+);
+ const string appUrlEnvKey = "MESSAGE_SERVICE_URL";
+string? appUrl = Environment.GetEnvironmentVariable(appUrlEnvKey);
+
+
+if (string.IsNullOrWhiteSpace(appUrl))
+{
+    throw new Exception($"{appUrlEnvKey} is not set in environment variables");
+}
 
 builder.Services.AddGrpc();
 var app = builder.Build();
 
 app.MapGrpcService<MessageServiceInternal>();
 
-app.Run();
+app.Run(appUrl.Trim());
