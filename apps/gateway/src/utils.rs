9600fa2ed5e234ab471bf834b4e3c8015750a38c
Darosss <darosss12@proton.me>
2026-08-21 14:31:08 +0200

    feat(gateway/utils): add get_token_data & add_user_id_to_request utils

---
+++ apps/gateway/src/utils.rs
@@ -0,0 +1,34 @@
+use tonic::{Request, Status, metadata::MetadataValue};
+
+use crate::{
+    accounts::{AuthService, ValidateTokenBody},
+    app_error::AppError,
+    pb::auth::ValidateTokenResponse,
+};
+
+pub async fn get_token_data(
+    auth_service: AuthService,
+    token: String,
+) -> Result<ValidateTokenResponse, AppError> {
+    if token.trim() == "" {
+        return Err(AppError::MessageService(Status::unauthenticated(
+            "You are not allowed to view messages of that channel",
+        )));
+    }
+    let user_data = auth_service
+        .validate_token(ValidateTokenBody { token })
+        .await?;
+    if !user_data.is_valid {
+        return Err(AppError::MessageService(Status::unauthenticated(
+            "Your token expired. Please log-in again",
+        )));
+    }
+    return Ok(user_data);
+}
+
+pub fn add_user_id_to_request<T>(mut request: Request<T>, user_id: i32) -> Request<T> {
+    let user_id_val = MetadataValue::from(user_id);
+
+    request.metadata_mut().insert("x-user-id", user_id_val);
+    return request;
+}
