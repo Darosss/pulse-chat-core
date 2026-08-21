d71f4fe5d8fda94614057cd8b1031e8fd246e659
Darosss <darosss12@proton.me>
2026-08-21 14:37:35 +0200

    feat(proto): add generated .proto types * accounts: - update message
    * message:
    - add Auth.cs and AuthGrpc.cs

--- apps/accounts/src/pb/message_pb2.pyi
+++ apps/accounts/src/pb/message_pb2.pyi
@@ -7,14 +7,12 @@ from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class CreateMessageRequest(_message.Message):
-    __slots__ = ("channel_id", "user_id", "content")
+    __slots__ = ("channel_id", "content")
     CHANNEL_ID_FIELD_NUMBER: _ClassVar[int]
-    USER_ID_FIELD_NUMBER: _ClassVar[int]
     CONTENT_FIELD_NUMBER: _ClassVar[int]
     channel_id: int
-    user_id: int
     content: str
-    def __init__(self, channel_id: _Optional[int] = ..., user_id: _Optional[int] = ..., content: _Optional[str] = ...) -> None: ...
+    def __init__(self, channel_id: _Optional[int] = ..., content: _Optional[str] = ...) -> None: ...
 
 class HistoryRequest(_message.Message):
     __slots__ = ("channel_id", "limit")
